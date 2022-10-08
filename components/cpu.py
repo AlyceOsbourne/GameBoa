cfa5cfe185998420023fdc84c5d1db596af215aa
Alyce <Alyceosbourne@outlook.com>
2022-10-08 11:35:24 +0100

    reduced repetition in execution end decode functions in the CPU

--- components/cpu.py
+++ components/cpu.py
@@ -1,11 +1,12 @@
 # gameboy cpu
+import operator
 import sys
 import time
 from dataclasses import dataclass
 
 import constants
 from cartridge import Cartridge
-import operator
+
 
 # we need a timer class that can be ticked at a sonsitent rate
 class Timer:
@@ -72,13 +73,37 @@ class Register:
         self.l = value & 0xFF
 
     def __str__(self):
-        return f'AF: {self.af:04X} BC: {self.bc:04X} DE: {self.de:04X} HL: {self.hl:04X} SP: {self.sp:04X} PC: {self.pc:04X}'
+        out = ''
+        for key, value in {
+            'A': self.a,
+            'B': self.b,
+            'C': self.c,
+            'D': self.d,
+            'E': self.e,
+            'H': self.h,
+            'L': self.l,
+            'F': self.f,
+            'SP': self.sp,
+            'PC': self.pc,
+
+        }.items():
+            out += f'{key}: {value:04X} '
+        return out
+
+
+class MMU:
+    ...
+
+
+class PPU:
+    ...
 
 
 class CPU:
     timer: Timer = Timer()
     cartridge: Cartridge = None
-    ppu = bytearray(0x2000)
+    mmu: MMU
+    ppu: PPU
     wram = bytearray(0x2000)
     vram = bytearray(0x2000)
     eram = bytearray(0x2000)
@@ -99,40 +124,29 @@ class CPU:
     def read_address(self, address: int):
         match address:
             case constants.MemoryMapRanges.ROM:
-                constants.logger.debug(f'ROM: {address:04X}')
                 return self.cartridge.read_rom(address)
             case constants.MemoryMapRanges.VRAM:
-                constants.logger.debug(f'VRAM: {address:04X}')
                 return self.vram[address - constants.MemoryMapRanges.VRAM.start]
             case constants.MemoryMapRanges.ERAM:
-                constants.logger.debug(f'ERAM: {address:04X}')
                 return self.eram[address - constants.MemoryMapRanges.ERAM.start]
             case constants.MemoryMapRanges.WRAM:
-                constants.logger.debug(f'WRAM: {address:04X}')
                 return self.wram[address - constants.MemoryMapRanges.WRAM.start]
             case constants.MemoryMapRanges.OAM:
-                constants.logger.debug(f'OAM: {address:04X}')
                 return self.oam[address - constants.MemoryMapRanges.OAM.start]
             case constants.MemoryMapRanges.HRAM:
-                constants.logger.debug(f'HRAM: {address:04X}')
                 return self.hram[address - constants.MemoryMapRanges.HRAM.start]
             case constants.MemoryMapRanges.IE:
-                constants.logger.debug(f'IE: {address:04X}')
                 return self.ime
             case constants.MemoryMapRanges.IF:
-                constants.logger.debug(f'IF: {address:04X}')
                 return self.ime_delay
             case constants.MemoryMapRanges.PPU:
-                constants.logger.debug(f'PPU: {address:04X}')
-                return self.ppu[address - constants.MemoryMapRanges.PPU.start]
+                #return self.ppu[address - constants.MemoryMapRanges.PPU.start]
+                pass
             case constants.MemoryMapRanges.IO:
-                constants.logger.debug(f'IO: {address:04X}')
                 return self.io[address - constants.MemoryMapRanges.IO.start]
             case constants.MemoryMapRanges.ECHO:
-                constants.logger.debug(f'ECHO: {address:04X}')
                 return self.read_address(address - 0x2000)
             case constants.MemoryMapRanges.UNUSED:
-                constants.logger.debug(f'UNUSED: {address:04X}')
                 return 0xFF
             case _:
                 raise Exception(f"Invalid address: {hex(address)}")
@@ -140,37 +154,28 @@ class CPU:
     def write_address(self, address: int, value: int):
         match address:
             case constants.MemoryMapRanges.ROM:
-                constants.logger.debug(f'ROM: {address:04X}')
                 self.cartridge.write_rom(address, value)
             case constants.MemoryMapRanges.VRAM:
-                constants.logger.debug(f'VRAM: {address:04X}')
                 self.vram[address - constants.MemoryMapRanges.VRAM.start] = value
             case constants.MemoryMapRanges.ERAM:
-                constants.logger.debug(f'ERAM: {address:04X}')
                 self.eram[address - constants.MemoryMapRanges.ERAM.start] = value
             case constants.MemoryMapRanges.WRAM:
-                constants.logger.debug(f'WRAM: {address:04X}')
                 self.wram[address - constants.MemoryMapRanges.WRAM.start] = value
             case constants.MemoryMapRanges.OAM:
-                constants.logger.debug(f'OAM: {address:04X}')
                 self.oam[address - constants.MemoryMapRanges.OAM.start] = value
             case constants.MemoryMapRanges.HRAM:
-                constants.logger.debug(f'HRAM: {address:04X}')
                 self.hram[address - constants.MemoryMapRanges.HRAM.start] = value
             case constants.MemoryMapRanges.IE:
-                constants.logger.debug(f'IE: {address:04X}')
                 self.ime = bool(value)
             case constants.MemoryMapRanges.IF:
-                constants.logger.debug(f'IF: {address:04X}')
                 self.ime_delay = bool(value)
             case constants.MemoryMapRanges.PPU:
-                constants.logger.debug(f'PPU: {address:04X}')
-                self.ppu[address - constants.MemoryMapRanges.PPU.start] = value
+                # self.ppu[address - constants.MemoryMapRanges.PPU.start] = value
+                print('attempted to access PPU')
+                pass
             case constants.MemoryMapRanges.IO:
-                constants.logger.debug(f'IO: {address:04X}')
                 self.io[address - constants.MemoryMapRanges.IO.start] = value
             case constants.MemoryMapRanges.ECHO:
-                constants.logger.debug(f'ECHO: {address:04X}')
                 self.write_address(address - 0x2000, value)
             case constants.MemoryMapRanges.UNUSED:
                 pass
@@ -200,47 +205,45 @@ class CPU:
             if (value := locals()[attr]) is not None:
                 self.write_flag(attr, value)
 
-    def read_operator(self, operator: str):
-        match operator:
+    def read_operator(self, op: str):
+        match op:
             case 'A' | 'B' | 'C' | 'D' | 'E' | 'H' | 'L' | 'F' | 'AF' | 'BC' | 'DE' | 'HL' | 'SP' | 'PC':
-                return self.read_register(operator.lower())
+                return self.read_register(op.lower())
             case 'd16' | 'a16':
                 return self.fetch16()
             case 'a8' | 'r8' | 'd8':
                 return self.fetch()
             case '(BC)' | '(DE)' | '(HL)' | '(HL+)' | '(HL-)' | '(C)' | '(a16)' | '(a8)':
-                return self.read_address(self.read_operator(operator[1:-1]))
+                return self.read_address(self.read_operator(op[1:-1]))
             case 'Z' | 'N' | 'H' | 'C':
-                return self.read_flag(operator)
+                return self.read_flag(op)
             case 'HL+' | 'HL-':
                 value = self.read_operator('HL')
-                self.write_operator('HL', value + (1 if operator[-1] == '+' else -1))
+                self.write_operator('HL', value + (1 if op[-1] == '+' else -1))
                 return value
             case '38H':
                 return 0x38
-            case 'NZ':
-                return not self.read_flag('Z')
+            case 'NZ' | 'NC':
+                return not self.read_flag(op[1])
             case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15':
-                return int(operator)
+                return int(op)
             case None:
                 return None
             case _:
-                raise ValueError(f'Invalid operator: {repr(operator)}')
+                raise ValueError(f'Invalid operator: {repr(op)}')
 
-    def write_operator(self, operator: str, value: int | bool):
-        match operator:
+    def write_operator(self, op: str, value: int | bool):
+        match op:
             case 'A' | 'B' | 'C' | 'D' | 'E' | 'H' | 'L' | 'F' | 'AF' | 'BC' | 'DE' | 'HL' | 'SP' | 'PC':
-                self.write_register(operator.lower(), value)
+                self.write_register(op.lower(), value)
             case '(BC)' | '(DE)' | '(HL)' | '(HL+)' | '(HL-)' | '(C)' | '(a16)' | '(a8)':
-                self.write_address(self.read_operator(operator[1:-1]), value)
+                self.write_address(self.read_operator(op[1:-1]), value)
             case 'Z' | 'N' | 'H' | 'C':
-                self.write_flag(operator, value)
-            case 'a8' | 'r8' | 'd8':
-                self.write_address(self.read_operator(operator), value)
-            case 'a16' | 'd16':
-                self.write_address(self.read_operator(operator), value)
+                self.write_flag(op, value)
+            case 'a8' | 'r8' | 'd8' | 'a16' | 'd16':
+                self.write_address(self.read_operator(op), value)
             case 'NZ' | 'NC':
-                self.write_flag(operator[0], not value)
+                self.write_flag(op[0], not value)
             case '38H':
                 pass
             case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15':
@@ -261,7 +264,7 @@ class CPU:
                                         '13': 'L',
                                         '14': '(HL)',
                                         '15': 'A',
-                                    }[operator], value)
+                                    }[op], value)
             case 'HL+':
                 self.write_operator('HL', value + 1)
             case 'HL-':
@@ -269,7 +272,7 @@ class CPU:
             case None:
                 pass
             case _:
-                raise ValueError(f'Invalid operator: {operator}')
+                raise ValueError(f'Invalid operator: {op}')
 
     def fetch(self):
         pc = self.read_register('pc')
@@ -280,13 +283,11 @@ class CPU:
         return self.fetch() | (self.fetch() << 8)
 
     def decode(self, opcode: int):
-        constants.logger.debug(f'PC: {self.reg.pc:04X} | Opcode: {opcode} | CB: {self.is_cb}')
         if self.is_cb:
             self.is_cb = False
             return self.cb_instructions[opcode]
         else:
             return self.instructions[opcode]
-
 
     def execute(self, instruction: constants.Instruction):
         operator_1, operator_2 = instruction.operand1, instruction.operand2
@@ -302,8 +303,6 @@ class CPU:
             case 'EI' | 'DI':
                 self.ime = instruction.mnemonic == 'EI'
 
-
-
             case 'ADD' | 'ADC' | 'SUB' | 'SBC':
                 value_1 = self.read_operator(operator_1)
                 value_2 = self.read_operator(operator_2 if operator_2 is not None else 'A')
@@ -314,17 +313,21 @@ class CPU:
                 self.write_flags(
                     Z=result == 0,
                     N=do_add,
-                    H= (operator.lt if do_add else operator.gt)(result & 0xF, value_1 & 0xf),
-                    C= (operator.gt if do_add else operator.lt(result, 0xff if do_add else 0)
-                ))
+                    H=(operator.lt if do_add else operator.gt)(result & 0xF, value_1 & 0xf),
+                    C=(operator.gt if do_add else operator.lt(result, 0xff if do_add else 0)
+                       ))
 
             case 'AND' | 'OR' | 'XOR':
                 value_1 = self.read_operator(operator_1)
                 value_2 = self.read_operator(operator_2 if operator_2 is not None else 'A')
-                op = operator.and_ if instruction.mnemonic[0] == 'A' else operator.or_ if instruction.mnemonic[0] == 'O' else operator.xor
+                op = {
+                    'A': operator.and_,
+                    'O': operator.or_,
+                    'X': operator.xor,
+                }[instruction.mnemonic[0]]
                 res = op(value_1, value_2)
                 self.write_operator(operator_1, res)
-                self.write_flags(Z=res == 0, N=False, H= instruction.mnemonic[0] == 'A', C=False)
+                self.write_flags(Z=res == 0, N=False, H=instruction.mnemonic[0] == 'A', C=False)
 
             case 'CP' if operator_2 is None:
                 value = self.read_operator(operator_1)
@@ -364,7 +367,7 @@ class CPU:
                 self.write_flags(N=False, H=False, C=not self.read_flag('C'))
             case 'SCF':
                 self.write_flags(N=False, H=False, C=True)
-            # shifts
+
             case 'RC':
                 value = self.read_operator(operator_1)
                 self.write_operator(operator_1, value >> 1)
@@ -403,6 +406,7 @@ class CPU:
 
             case 'JR' if operator_2 is None:
                 self.write_operator('PC', self.read_operator('PC') + self.read_operator(operator_1))
+
             case 'JR':
                 self.write_operator(operator_1, self.read_operator(operator_1) + self.read_operator(operator_2))
 
@@ -410,16 +414,18 @@ class CPU:
                 self.write_operator('SP', self.read_operator('SP') - 2)
                 self.write_operator('PC', self.read_operator(operator_1))
 
-            # bit operations
             case 'BIT':
                 value = self.read_operator(operator_1)
                 self.write_flags(Z=(value & (1 << self.read_operator(operator_2))) == 0, N=False, H=True)
+
             case 'SET':
                 value = self.read_operator(operator_1)
                 self.write_operator(operator_1, value | (1 << self.read_operator(operator_2)))
+
             case 'RES':
                 value = self.read_operator(operator_1)
                 self.write_operator(operator_1, value & ~(1 << self.read_operator(operator_2)))
+
             case 'SWAP':
                 value = self.read_operator(operator_1)
                 self.write_operator(operator_1, ((value & 0xF) << 4) | ((value & 0xF0) >> 4))
@@ -439,7 +445,6 @@ class CPU:
         return self.execute(self.decode(self.fetch()))
 
     def run(self, cart: Cartridge):
-        constants.logger.debug('Running | ' + cart.title)
         self.cartridge = cart
         self.reset()
         while True:
@@ -456,10 +461,6 @@ class CPU:
         ]:
             self.write_register(register, value)
 
-    @staticmethod
-    def check_cb(opcode: int):
-        return opcode in [0xCB, 0xDDCB, 0xEDCB, 0xFDCB]
-
     def print_registers(self):
         print('Registers: ' + str(self.reg))
 
@@ -467,10 +468,29 @@ class CPU:
         print('Flags: ' + str(self.read_flags('ZNHC')))
 
 
-if __name__ == "__main__":
+def main(step_mode: bool = False):
     cpu = CPU()
     cpu.reset()
-    input()
-    for instruction in cpu.run(Cartridge('Tetris.gb')):
-        ...
-    print('Done')
+    cart = Cartridge(sys.argv[1])
+    time.sleep(1)
+    print('Loaded ' + cart.title)
+    input('Press enter to start')
+    print(chr(27) + "[2J", "\033[F" * 4, end="")
+    time.sleep(1)
+    for instruction in cpu.run(cart):
+        reg = str(cpu.reg)
+        flags = ""
+        for k, v in zip('ZNHC', cpu.read_flags('ZNHC')):
+            # the value should be hex like C3 of FF etc
+            flags += f"{k}:{v:2x} "
+        mnem = instruction.mnemonic
+        op1 = instruction.operand1 if instruction.operand1 else 'NONE'
+        op2 = instruction.operand2 if instruction.operand2 else 'NONE'
+        print(f"\r| REG: {reg} | FLAGS: {flags} | FUNC: {mnem:<4} | OP 1: {op1:<5} | OP 2: {op2:<5} | ",
+              flush=True, end="")
+        if step_mode:
+            input()
+
+
+if __name__ == '__main__':
+    main(False)
