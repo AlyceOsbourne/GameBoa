0594e63979943593bb33935a744e467acd2901fd
PedanticHacker <bostjan.xperia@gmail.com>
2022-10-16 03:20:38 +0200

    Add some new stuff

---
+++ roms/stub.py
@@ -0,0 +1,2 @@
+# This is a stub Python file to make the roms directory be non-empty,
+# hence be commited to GitHub.
---
+++ settings.py
@@ -0,0 +1,57 @@
+from PySide6.QtWidgets import QCheckBox, QDialogButtonBox, QTabWidget
+
+
+OK_BUTTON = QDialogButtonBox.StandardButton.Ok
+CANCEL_BUTTON = QDialogButtonBox.StandardButton.Cancel
+
+
+class SettingsDialogWindow(QDialog):
+    """A dialog window that provides changing GameBoa's settings."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.set_tabs()
+        self.set_options()
+        self.set_properties()
+        self.set_custom_layout()
+        self.connect_signals_with_slots()
+
+        self.exec()
+
+    def set_tabs(self) -> None:
+        """Sets a tab for each settings category."""
+        self.tab_widget = QTabWidget()
+        self.tab_widget.addTab(QWidget_derivative_here, "Text of the tab here")
+
+    def set_options(self) -> None:
+        """Sets options that represent GameBoa's settings."""
+        self.test_option = QCheckBox()
+        self.test_option.setText("Test")
+        self.test_option.setChecked(True)
+
+    def set_properties(self) -> None:
+        """
+        Sets specific properties for the dialog window.
+
+        > Sets the title to be Settings.
+        > Sets the standard buttons to be OK and Cancel.
+        """
+        self.setWindowTitle("Settings")
+
+        self.button_box = QDialogButtonBox()
+        self.button_box.setStandardButtons(OK_BUTTON | CANCEL_BUTTON)
+
+    def set_custom_layout(self) -> None:
+        """Sets a custom layout for the widgets on the dialog window."""
+        ...
+
+    def connect_signals_with_slots(self) -> None:
+        """Connects event signals with relevant slots as actions."""
+        self.accepted.connect(self.save_settings)
+        self.button_box.accepted.connect(self.accept)
+        self.button_box.rejected.connect(self.reject)
+
+    def save_settings(self) -> None:
+        """Saves GameBoa's settings to a specific file."""
+        ...
