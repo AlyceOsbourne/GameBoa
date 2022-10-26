f5b93b0c62305ff5e2bef029e6aab6417d1c43cb
PedanticHacker <bostjan.xperia@gmail.com>
2022-10-26 04:31:02 +0200

    Fix __metadata__.py

--- __metadata__.py
+++ __metadata__.py
@@ -1,30 +1,64 @@
-import colorama
 from datetime import date
 
-VERSION = MAJOR, MINOR, MICRO, NIGHTLY = 0, 0, 0, 13
-__version__ = f'{MAJOR}.{MINOR}.{MICRO}{f"-dev{NIGHTLY}" if NIGHTLY else "-release"}'
-__authors__ = "Alyce Osbourne", "Boštjan Mejak"
-__github__ = "https://github.com/AlyceOsbourne/GameBoaV2"
-__license__ = "MIT"
+import colorama
 
 
-metadata = f"""GameBoa {__version__}
-GitHub: {__github__}
-Authors: {", ".join(__authors__)}
-License: {__license__}
+__version__ = "0.0.1"
+__license__ = "MIT License"
+__copyright_year__ = date.today().year
+__authors__ = "Alyce Osbourne, Boštjan Mejak"
+__github_link__ = "https://github.com/AlyceOsbourne/GameBoa"
+__license_text__ = f"""
+Copyright {__copyright_year__} {__authors__}
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+“Software”), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-# make the metadata string rainbow
-for i, line in enumerate(metadata.splitlines()):
-    if i == 1:
-        metadata = metadata.replace(line, colorama.Fore.LIGHTBLUE_EX + line + colorama.Fore.RESET)
-    elif i == 2:
-        metadata = metadata.replace(line, colorama.Fore.LIGHTGREEN_EX + line + colorama.Fore.RESET)
-    elif i == 3:
-        metadata = metadata.replace(line, colorama.Fore.LIGHTYELLOW_EX + line + colorama.Fore.RESET)
-    elif i == 4:
-        metadata = metadata.replace(line, colorama.Fore.LIGHTCYAN_EX + line + colorama.Fore.RESET)
-    else:
-        metadata = metadata.replace(line, colorama.Fore.CYAN + line + colorama.Fore.RESET)
+metadata = f"""
+GameBoa {__version__}
+Authors: {__authors__}
+License: {__license__}
+GitHub: {__github_link__}
+"""
+
+for index, line in enumerate(metadata.splitlines()):
+    metadata = metadata.replace(
+        line, colorama.Fore.LIGHTMAGENTA_EX + line + colorama.Fore.RESET
+    )
+
+    if index == 1:
+        metadata = metadata.replace(
+            line, colorama.Fore.LIGHTBLUE_EX + line + colorama.Fore.RESET
+        )
+    elif index == 2:
+        metadata = metadata.replace(
+            line, colorama.Fore.LIGHTGREEN_EX + line + colorama.Fore.RESET
+        )
+    elif index == 3:
+        metadata = metadata.replace(
+            line, colorama.Fore.LIGHTYELLOW_EX + line + colorama.Fore.RESET
+        )
+    elif index == 4:
+        metadata = metadata.replace(
+            line, colorama.Fore.LIGHTCYAN_EX + line + colorama.Fore.RESET
+        )
 
 print(metadata)
+print(__license_text__)
