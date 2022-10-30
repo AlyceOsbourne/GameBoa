4ac46e2f9bf50eb8c610510859ddfaab827166bc
PedanticHacker <bostjan.xperia@gmail.com>
2022-10-30 11:15:02 +0100

    Dusting the old book shelves...

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,11 +2,13 @@ import unittest
 from memory import TestMemory
 from register import TestRegister
 
+
 def suite():
     _suite = unittest.TestSuite()
     _suite.addTest(unittest.makeSuite(TestMemory))
     _suite.addTest(unittest.makeSuite(TestRegister))
     return _suite
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
